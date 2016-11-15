538f08660d0d5a7deca16db2e927be05eea35043
nuts4nets <nuts4nets@nuts4netss-MacBook-Pro.local>
2016-11-15 12:07:08 -0500

    updates

--- mnist/main.py
+++ mnist/main.py
@@ -10,12 +10,29 @@ import lasagne
 
 import matplotlib.pyplot as plt
 
+from viz import plot_images
 
 class ConsiderConstant(theano.compile.ViewOp):
     def grad(self, args, g_outs):
         return [T.zeros_like(g_out) for g_out in g_outs]
 
 consider_constant = ConsiderConstant()
+
+import cPickle as pickle
+import gzip
+from random import randint
+data = pickle.load(gzip.open("mnist.pkl.gz", "r"))
+
+trx = data[0][0]
+
+
+#output is 16 x 784
+def sample_data():
+    return trx[0:16]
+
+
+
+
 
 '''
 Start with two hidden layer MLP.  
@@ -27,7 +44,7 @@ def init_params():
 
     params = {}
 
-    params["h1_W"] = 0.1 * rng.normal(size = (1,256)).astype('float32')
+    params["h1_W"] = 0.1 * rng.normal(size = (28*28,256)).astype('float32')
     params["h1_b"] = 0.0 * rng.normal(size = (256,)).astype('float32')
     params["h2_W"] = 0.1 * rng.normal(size = (256,256)).astype('float32')
     params["h2_b"] = 0.0 * rng.normal(size = (256,)).astype('float32')
@@ -43,9 +60,9 @@ def params_shared(params):
 
 def score_network(params, x):
 
-    h1 = T.tanh(T.dot(x, params["h1_W"]) + params['h1_b'])
+    h1 = T.maximum(0.0, T.dot(x, params["h1_W"]) + params['h1_b'])
 
-    h2 = T.tanh(T.dot(h1, params['h2_W']) + params['h2_b'])
+    h2 = T.maximum(0.0, T.dot(h1, params['h2_W']) + params['h2_b'])
 
     s = T.dot(h2, params['s_W'])
 
@@ -64,11 +81,13 @@ if __name__ == "__main__":
     s_xl = score_network(params, xl)
     s_xq = score_network(params, xq)
 
-    xn = xl + 1.0 * T.grad(T.sum(s_xl), xl)
+    xn = xl + 0.1 * T.grad(T.sum(s_xl), xl)
+
+    xb = xl - 0.1 * T.grad(T.sum(s_xl), xl)
 
     s_xn = score_network(params, xn)
 
-    loss = T.maximum(0.0, s_xl - s_xn + 0.1) * 0.0 + T.maximum(0.0, s_xl - s_xq + 0.1)
+    loss = T.maximum(0.0, s_xl - s_xn + 0.1) + T.maximum(0.0, s_xl - s_xq + 0.1)
     
     loss = T.mean(loss)
 
@@ -78,37 +97,37 @@ if __name__ == "__main__":
 
     get_s = theano.function([xl], s_xl)
 
-    num_steps = 100
+    reverse_step = theano.function([xl], [xb, s_xl])
+
+    num_steps = 150
 
     for iteration in range(0,200000):
 
-        xu = rng.uniform(0.0,0.01,size=(16,1)).astype('float32')
+        xu = sample_data()
 
         if iteration % 100 == 0:
             print "===================================="
 
         for step in range(0,num_steps):
 
-            if iteration % 100 == 0:
-                print step, xu[0]
-                print "s", s_xl[0]
-
-            xq = rng.uniform(-2.5,2.5,size=(16,1)).astype('float32')
+            xq = rng.uniform(-1.5,1.5,size=(16,28*28)).astype('float32')
             out = train(xu,xq)
             xu = out['xn']
             s_xl = out['s_xl']
 
+            if iteration % 100 == 0:
+                print step, "s", s_xl[0]
+                if step == num_steps-1:
+                    print "s for prior", get_s(xq)
+                    print "xu shape", xu.shape
+                plot_images(xu.reshape(16,1,28,28), "plots/imagewalk_step_" + str(step))
 
-        if iteration % (100*100/num_steps) == 0:
-            print "SHOWING S VALUES OVER RANGE"
-            v = np.linspace(-2.5,2.5,100).reshape(100,1).astype('float32')
 
-            s = get_s(v)
+        if iteration % 2000 == 0:
+            print "DOING GENERATION"
+            for step in range(0, num_steps):
+                res = reverse_step(xu)
+                print step, "score", res[1][0]
+                xu = res[0]
 
-            for i in range(0, v.shape[0]):
-                print i, v[i], s[i]
-
-            plt.title("Num steps walkback: " + str(num_steps) + " #updates: " + str(iteration*num_steps))
-            plt.plot(s)
-            plt.show()
-
+                plot_images(xu.reshape(16,1,28,28), "plots/gen/step_" + str(step))
